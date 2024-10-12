2fe31ff057c87169f2753d420670e36af4fcc681
ankunzt12 <131801317+ankunzt12@users.noreply.github.com>
2024-10-12 10:36:59 +0700

    lovetime.js

--- public/js/lovetime.js
+++ public/js/lovetime.js
@@ -1,1 +1,13 @@
-
+const ThanhDieu_List = [
+"https://files.catbox.moe/zuwsxa.mp3",
+  ];
+  //Random music
+  let index = Math.floor(Math.random() * ThanhDieu_List.length);
+  const audio = new Audio(ThanhDieu_List[index]);
+  function ThanhDieuAudio() {audio.play();}
+  document.addEventListener("click", ThanhDieuAudio);
+  audio.addEventListener("ended", function() {
+    index = (index + 1) % ThanhDieu_List.length;
+    audio.src = ThanhDieu_List[index];
+    audio.play();
+  });
